f7a3ccc32d7d76332fa7b130be51e396fe15e136
Trần Thiện Thanh <kurotsmile@gmail.com>
2024-06-26 22:53:49 +0700

    Update Pi manager
    Update Pi manager

--- Scripts/Pin.cs
+++ Scripts/Pin.cs
@@ -20,11 +20,18 @@ public class Pin : MonoBehaviour
 
         for (int i = 0; i < pin_color.Length; i++)
         {
+            var index_pin = i;
             Carrot_Box_Item item_pin = box.create_item("item_pin_" + i);
             item_pin.set_icon(app.sp_icon_pin);
             item_pin.img_icon.color = pin_color[i];
             item_pin.set_title("Pin " + (i + 1));
             item_pin.set_tip("Pin for box");
+            item_pin.set_act(() => Set_Pin_for_box(index_pin));
         }
+    }
+
+    public void Set_Pin_for_box(int index_pin)
+    {
+        app.carrot.Show_msg("Pin:" + index_pin);
     }
 }
